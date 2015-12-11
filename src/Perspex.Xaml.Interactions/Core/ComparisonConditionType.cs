6df1c931f3f82095dbb7d72e92ddb41be867bbe5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-12-11 14:55:54 +0100

    Update ComparisonConditionType.cs

--- src/Perspex.Xaml.Interactions/Core/ComparisonConditionType.cs
+++ src/Perspex.Xaml.Interactions/Core/ComparisonConditionType.cs
@@ -4,15 +4,33 @@
 namespace Perspex.Xaml.Interactions.Core
 {
     /// <summary>
-	/// Represents one ternary condition.
-	/// </summary>
-	public enum ComparisonConditionType
+    /// Represents one ternary condition.
+    /// </summary>
+    public enum ComparisonConditionType
     {
+        /// <summary>
+        /// Specifies an equal condition.
+        /// </summary>
         Equal,
+        /// <summary>
+        /// Specifies a not equal condition.
+        /// </summary>
         NotEqual,
+        /// <summary>
+        /// Specifies a less than condition.
+        /// </summary>
         LessThan,
+        /// <summary>
+        /// Specifies a less than or equal condition.
+        /// </summary>
         LessThanOrEqual,
+        /// <summary>
+        /// Specifies a greater than condition.
+        /// </summary>
         GreaterThan,
+        /// <summary>
+        /// Specifies a greater than or equal condition.
+        /// </summary>
         GreaterThanOrEqual
     }
 }
