7448df6d7ae0fba2c1f161d69b80dd2a2b629655
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-20 20:45:03 +0100

    Removed using

--- src/Perspex.Xaml.Interactions/Properties/AssemblyInfo.cs
+++ src/Perspex.Xaml.Interactions/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
 using System.Reflection;
-using Perspex.Metadata;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Perspex.Xaml.Interactions")]
