43439b93afcb719265f62f0832198fb7f69bef6d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-05 13:03:30 +0100

    Added PopupAction class

---
+++ samples/XamlTestApplicationPcl/Actions/PopupAction.cs
@@ -0,0 +1,27 @@
+﻿using Perspex;
+using Perspex.Controls;
+using Perspex.Controls.Primitives;
+using Perspex.Media;
+using Perspex.Xaml.Interactivity;
+
+namespace XamlTestApplication.Actions
+{
+    public class PopupAction : PerspexObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            new Popup()
+            {
+                PlacementMode = PlacementMode.Pointer,
+                PlacementTarget = sender as Control,
+                StaysOpen = false,
+                Child = new TextBlock
+                {
+                    Text = "Hello from custom action!",
+                    Background = Brushes.Gray
+                }
+            }.Open();
+            return null;
+        }
+    }
+}
