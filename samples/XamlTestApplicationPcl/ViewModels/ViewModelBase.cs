aa9bf9fcc37e94eb7995cf70a6f72f682e14c5fd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-05 09:10:31 +0100

    Added ViewModelBase class

---
+++ samples/XamlTestApplicationPcl/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System.ComponentModel;
+
+namespace XamlTestApplication.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
