1edb7ab05bd8eb799c390b447917e6ad3ec04a69
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-05 09:13:33 +0100

    Added CallMethodAction sample

--- samples/XamlTestApplicationPcl/ViewModels/MainWindowViewModel.cs
+++ samples/XamlTestApplicationPcl/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,30 @@
 ﻿// Copyright (c) The Perspex Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
-using System.Collections.Generic;
-
 namespace XamlTestApplication.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
+        public int Count { get; set; }
+
         public MainWindowViewModel()
         {
+            Count = 0;
+        }
+
+        public void IncrementCount()
+        {
+            Count++;
+            OnPropertyChanged(nameof(Count));
+        }
+
+        public void DecrementCount(object sender, object parameter)
+        {
+            if (Count > 0)
+            {
+                Count--;
+                OnPropertyChanged(nameof(Count));
+            }
         }
     }
 }
