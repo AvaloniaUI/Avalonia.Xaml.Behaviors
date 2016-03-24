a78eeae72e40d34e251fe2bb608a38d323d166ea
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-24 19:26:24 +0100

    Added ShowPointerPositionBehavior class

---
+++ samples/XamlTestApplicationPcl/Behaviors/ShowPointerPositionBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Perspex;
+using Perspex.Controls;
+using Perspex.Xaml.Interactivity;
+
+namespace XamlTestApplication.Behaviors
+{
+    public sealed class ShowPointerPositionBehavior : Behavior<Control>
+    {
+        public static readonly PerspexProperty TargetTextBlockProperty =
+            PerspexProperty.Register<ShowPointerPositionBehavior, TextBlock>(nameof(TargetTextBlock));
+
+        public TextBlock TargetTextBlock
+        {
+            get { return (TextBlock)this.GetValue(TargetTextBlockProperty); }
+            set { this.SetValue(TargetTextBlockProperty, value); }
+        }
+
+        private void AssociatedObject_PointerMoved(object sender, Perspex.Input.PointerEventArgs e)
+        {
+            if (TargetTextBlock != null)
+            {
+                TargetTextBlock.Text = e.GetPosition(this.AssociatedObject).ToString();
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.PointerMoved += AssociatedObject_PointerMoved;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.PointerMoved -= AssociatedObject_PointerMoved;
+        }
+    }
+}
