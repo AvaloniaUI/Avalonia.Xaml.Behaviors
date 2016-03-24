c410d82fae6179f2e0459944c65e988b989b9513
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-24 21:09:28 +0100

    Inherit DataContext from sender

--- samples/XamlTestApplicationPcl/Actions/PopupAction.cs
+++ samples/XamlTestApplicationPcl/Actions/PopupAction.cs
@@ -3,18 +3,19 @@ using Perspex.Controls;
 using Perspex.Controls.Primitives;
 using Perspex.Metadata;
 using Perspex.Xaml.Interactivity;
+using System;
 
 namespace XamlTestApplication.Actions
 {
     public class PopupAction : PerspexObject, IAction
     {
-        public static readonly PerspexProperty ChildProperty =
+        public static readonly PerspexProperty<Control> ChildProperty =
             PerspexProperty.Register<PopupAction, Control>(nameof(Child));
 
         [Content]
         public Control Child
         {
-            get { return (Control)this.GetValue(ChildProperty); }
+            get { return this.GetValue(ChildProperty); }
             set { this.SetValue(ChildProperty, value); }
         }
 
@@ -30,10 +31,31 @@ namespace XamlTestApplication.Actions
                     PlacementTarget = sender as Control,
                     StaysOpen = false
                 };
+
+                var control = sender as IControl;
+                if (control != null)
+                {
+                    BindToDataContext(control, _popup);
+                }
             }
             _popup.Child = Child;
             _popup.Open();
             return null;
+        }
+
+        private static void BindToDataContext(IControl source, IControl target)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            var data = source.GetObservable(Control.DataContextProperty);
+            if (data != null)
+            {
+                target.Bind(Control.DataContextProperty, data);
+            }
         }
     }
 }
