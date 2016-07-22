5c60dd9901d552a5cf29d49e4c7e0490ab5ff83e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-22 20:49:00 +0200

    Added XmlnsDefinition support

--- src/Avalonia.Xaml.Interactions/Properties/AssemblyInfo.cs
+++ src/Avalonia.Xaml.Interactions/Properties/AssemblyInfo.cs
@@ -3,6 +3,10 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Avalonia.Metadata;
 
 [assembly: AssemblyTitle("Avalonia.Xaml.Interactions")]
 [assembly: InternalsVisibleTo("Avalonia.Xaml.Interactions.UnitTests")]
+
+[assembly: XmlnsDefinition("https://github.com/xamlbehaviors", "Avalonia.Xaml.Interactions")]
+[assembly: XmlnsDefinition("https://github.com/xamlbehaviors", "Avalonia.Xaml.Interactions.Core")]
--- src/Avalonia.Xaml.Interactivity/Properties/AssemblyInfo.cs
+++ src/Avalonia.Xaml.Interactivity/Properties/AssemblyInfo.cs
@@ -3,6 +3,9 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Avalonia.Metadata;
 
 [assembly: AssemblyTitle("Avalonia.Xaml.Interactivity")]
 [assembly: InternalsVisibleTo("Avalonia.Xaml.Interactivity.UnitTests")]
+
+[assembly: XmlnsDefinition("https://github.com/xamlbehaviors", "Avalonia.Xaml.Interactivity")]
