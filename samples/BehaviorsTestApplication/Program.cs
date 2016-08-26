781410389aa138e2044212d4b9b71a73c5ed790e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-26 16:41:50 +0200

    Updated Avalonia

--- samples/BehaviorsTestApplication/Program.cs
+++ samples/BehaviorsTestApplication/Program.cs
@@ -1,11 +1,11 @@
 // Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
-using BehaviorsTestApplication.Views;
-using Avalonia.Controls;
-using Avalonia.Logging.Serilog;
-using Serilog;
 using System.Windows.Threading;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using BehaviorsTestApplication.Views;
+using Serilog;
 
 namespace BehaviorsTestApplication
 {
