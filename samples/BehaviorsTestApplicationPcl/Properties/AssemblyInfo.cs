01e4047691389aa65fa45eb769301c3ba646035c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-12-25 14:24:22 +0100

    Updated source files headers

--- samples/BehaviorsTestApplicationPcl/Properties/AssemblyInfo.cs
+++ samples/BehaviorsTestApplicationPcl/Properties/AssemblyInfo.cs
@@ -1,11 +1,9 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
 using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("BehaviorsTestApplicationPcl")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -15,16 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- tests/Avalonia.Xaml.Interactivity.UnitTests/TestUitilties.cs
+++ tests/Avalonia.Xaml.Interactivity.UnitTests/TestUitilties.cs
@@ -1,3 +1,6 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
 using System;
 using Xunit;
 
