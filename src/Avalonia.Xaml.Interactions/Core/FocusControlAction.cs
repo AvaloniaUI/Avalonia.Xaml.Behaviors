972966d60da85f4d9164b9748c92c840feb57c69
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-24 09:37:07 +0200

    Create FocusControlAction.cs
    [skip ci]

---
+++ src/Avalonia.Xaml.Interactions/Core/FocusControlAction.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Core
+{
+    /// <summary>
+    /// Focuses the associated control when executed.
+    /// </summary>
+    public sealed class FocusControlAction : AvaloniaObject, IAction
+    {
+        /// <inheritdoc/>
+        public object Execute(object sender, object parameter)
+        {
+            (sender as Control)?.Focus();
+            return null;
+        }
+    }
+}
