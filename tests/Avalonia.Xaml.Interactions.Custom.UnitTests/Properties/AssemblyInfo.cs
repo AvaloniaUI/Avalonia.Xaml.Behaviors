28db7ec26f5b3de818fd9890312ca07e0ca5063c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-24 11:06:29 +0200

    Create AssemblyInfo.cs
    [skip ci]

---
+++ tests/Avalonia.Xaml.Interactions.Custom.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System.Reflection;
+using Xunit;
+
+[assembly: AssemblyTitle("Avalonia.Xaml.Interactions.Custom.UnitTests")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
