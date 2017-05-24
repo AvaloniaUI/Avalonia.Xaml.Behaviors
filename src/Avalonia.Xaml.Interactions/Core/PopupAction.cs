5cca14aaf56f1e515a251f513ad69cec391ece10
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-24 10:11:37 +0200

    Update PopupAction.cs
    [skip ci]

--- src/Avalonia.Xaml.Interactions/Core/PopupAction.cs
+++ src/Avalonia.Xaml.Interactions/Core/PopupAction.cs
@@ -34,7 +34,12 @@ namespace Avalonia.Xaml.Interactions.Core
             set { this.SetValue(ChildProperty, value); }
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Executes the action.
+        /// </summary>
+        /// <param name="sender">The <see cref="System.Object"/> that is passed to the action by the behavior. Generally this is <seealso cref="IBehavior.AssociatedObject"/> or a target object.</param>
+        /// <param name="parameter">The value of this parameter is determined by the caller.</param>
+        /// <returns>Returns null after executed.</returns>
         public object Execute(object sender, object parameter)
         {
             if (_popup == null)
