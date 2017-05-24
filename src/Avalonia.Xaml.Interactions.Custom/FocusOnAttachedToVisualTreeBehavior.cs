fbb574a682284d5f0fc6f5c31f4374f4df7341ed
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-24 10:59:38 +0200

    Update and rename src/Avalonia.Xaml.Interactions/Core/FocusOnAttachedToVisualTreeBehavior.cs to src/Avalonia.Xaml.Interactions.Custom/FocusOnAttachedToVisualTreeBehavior.cs
    [skip ci]

--- src/Avalonia.Xaml.Interactions.Custom/FocusOnAttachedToVisualTreeBehavior.cs
+++ src/Avalonia.Xaml.Interactions.Custom/FocusOnAttachedToVisualTreeBehavior.cs
@@ -5,7 +5,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Xaml.Interactivity;
 
-namespace Avalonia.Xaml.Interactions.Core
+namespace Avalonia.Xaml.Interactions.Custom
 {
     /// <summary>
     /// Focuses the AssociatedObject when attached to visual tree.
