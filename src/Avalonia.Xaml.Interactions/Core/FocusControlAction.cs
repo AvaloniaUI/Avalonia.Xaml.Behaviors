6d16db9310911d06ad9be139bf02f32c6452298e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-24 10:13:55 +0200

    Update FocusControlAction.cs
    [skip ci]

--- src/Avalonia.Xaml.Interactions/Core/FocusControlAction.cs
+++ src/Avalonia.Xaml.Interactions/Core/FocusControlAction.cs
@@ -12,7 +12,12 @@ namespace Avalonia.Xaml.Interactions.Core
     /// </summary>
     public sealed class FocusControlAction : AvaloniaObject, IAction
     {
-        /// <inheritdoc/>
+        /// <summary>
+        /// Executes the action.
+        /// </summary>
+        /// <param name="sender">The <see cref="System.Object"/> that is passed to the action by the behavior. Generally this is <seealso cref="IBehavior.AssociatedObject"/> or a target object.</param>
+        /// <param name="parameter">The value of this parameter is determined by the caller.</param>
+        /// <returns>Returns null after executed.</returns>
         public object Execute(object sender, object parameter)
         {
             (sender as Control)?.Focus();
