640debb608421134bfc8d1d0488ae7699ef389b7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-25 08:30:46 +0200

    Update and rename Stubs.cs to StubBehavior.cs
    [skip ci]

--- tests/Avalonia.Xaml.Interactivity.UnitTests/StubBehavior.cs
+++ tests/Avalonia.Xaml.Interactivity.UnitTests/StubBehavior.cs
@@ -53,45 +53,4 @@ namespace Avalonia.Xaml.Interactivity.UnitTests
             return Interaction.ExecuteActions(sender, Actions, parameter);
         }
     }
-
-    public class StubAction : AvaloniaObject, IAction
-    {
-        private readonly object returnValue;
-
-        public StubAction()
-        {
-            returnValue = null;
-        }
-
-        public StubAction(object returnValue)
-        {
-            this.returnValue = returnValue;
-        }
-
-        public object Sender
-        {
-            get;
-            private set;
-        }
-
-        public object Parameter
-        {
-            get;
-            private set;
-        }
-
-        public int ExecuteCount
-        {
-            get;
-            private set;
-        }
-
-        public object Execute(object sender, object parameter)
-        {
-            ExecuteCount++;
-            Sender = sender;
-            Parameter = parameter;
-            return returnValue;
-        }
-    }
 }
