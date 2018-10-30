ef62aa43c56185da173f008c1ee704b43474a29e
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-10-30 07:09:03 +0000

    Updated version

--- src/Shared/SharedAssemblyInfo.cs
+++ src/Shared/SharedAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.6.2")]
-[assembly: AssemblyFileVersion("0.6.2")]
-[assembly: AssemblyInformationalVersion("0.6.2")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
