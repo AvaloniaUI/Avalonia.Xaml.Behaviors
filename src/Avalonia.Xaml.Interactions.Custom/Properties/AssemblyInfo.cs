bc6714df52840cbd4094526b9c02044a69b39f3d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-12-03 19:03:20 +0100

    Added XmlnsDefinition

--- src/Avalonia.Xaml.Interactions.Custom/Properties/AssemblyInfo.cs
+++ src/Avalonia.Xaml.Interactions.Custom/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+using Avalonia.Metadata;
 
 [assembly: InternalsVisibleTo("Avalonia.Xaml.Interactions.Custom.UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002940ed211918fcf63c506fad1d3f7f958b21ff8f06fd2089398296173f9ca93a69b9b380a828bf13fa80d1745beeb917ec3692f4d10e44b4c941619fc7bbd5052b26880697e6fa3f0ce322c4fa902d20b67a48b4144371218f6d39ad39145ea1fe5484052dd51a2ee62af3acd0759bcf92aaefec03978ded3cfaa84798e92de8")]
+
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactions.Custom")]
