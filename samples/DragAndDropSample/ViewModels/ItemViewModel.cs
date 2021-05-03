289d8a85742a572507d4a5a92a105f70ec680fc0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-03 20:45:19 +0200

    Added DragAndDropSample

---
+++ samples/DragAndDropSample/ViewModels/ItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace DragAndDropSample.ViewModels
+{
+    public class ItemViewModel : ViewModelBase
+    {
+        private string? _title;
+
+        public string? Title
+        {
+            get => _title;
+            set => this.RaiseAndSetIfChanged(ref _title, value);
+        }
+    }
+}
