23e0c0c04ec7e7a109d8c37c30055d7fc4fa40bd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-07 15:42:24 +0200

    Added DraggableDemo

---
+++ samples/DraggableDemo/Item.cs
@@ -0,0 +1,13 @@
+namespace DraggableDemo
+{
+    public class Item
+    {
+        public string? Title { get; set; }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public override string? ToString() => Title;
+    }
+}
