6c525b626918786d1a7ee195b3d1d0bb1016aafb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-26 22:25:45 +0200

    Add ValueChangedTriggerBehaviorControl

---
+++ samples/BehaviorsTestApplication/Pages/ValueChangedTriggerBehaviorControl.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Pages
+{
+    public class ValueChangedTriggerBehaviorControl : UserControl
+    {
+        public ValueChangedTriggerBehaviorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
