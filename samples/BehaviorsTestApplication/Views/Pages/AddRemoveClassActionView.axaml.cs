c21d284942907d5756d3819fc408dacbf72c7c2a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-08 13:38:39 +0200

    Add AddRemoveClassActionView

---
+++ samples/BehaviorsTestApplication/Views/Pages/AddRemoveClassActionView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages
+{
+    public class AddRemoveClassActionView : UserControl
+    {
+        public AddRemoveClassActionView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
