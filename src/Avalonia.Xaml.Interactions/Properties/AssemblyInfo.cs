3166c6944dedcfd90c48d160d70e475b39e405fa
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-09 12:06:16 +0200

    Update Base.props

--- src/Avalonia.Xaml.Interactions/Properties/AssemblyInfo.cs
+++ src/Avalonia.Xaml.Interactions/Properties/AssemblyInfo.cs
@@ -8,3 +8,4 @@ using Avalonia.Metadata;
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactions.Custom")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactions.DragAndDrop")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactions.Draggable")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactions.Responsive")]
