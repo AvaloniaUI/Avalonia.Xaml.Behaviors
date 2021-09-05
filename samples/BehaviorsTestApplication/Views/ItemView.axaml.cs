41898b34a452dee23750b53efa270228c4fe1aa0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-05 21:14:09 +0200

    Add ItemView

---
+++ samples/BehaviorsTestApplication/Views/ItemView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views
+{
+    public class ItemView : UserControl
+    {
+        public ItemView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
