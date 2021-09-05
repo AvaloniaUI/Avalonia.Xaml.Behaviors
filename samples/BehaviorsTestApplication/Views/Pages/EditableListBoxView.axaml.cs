bdd3cb20aa95518cf0231c5fe2861d331f3a7832
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-05 20:35:24 +0200

    Add EditableListBoxView sample page

---
+++ samples/BehaviorsTestApplication/Views/Pages/EditableListBoxView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages
+{
+    public class EditableListBoxView : UserControl
+    {
+        public EditableListBoxView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
