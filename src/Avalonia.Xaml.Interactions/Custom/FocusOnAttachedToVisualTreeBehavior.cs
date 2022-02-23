2f972ebb0066a2a4235126da7e103f684de1c777
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-02-23 10:26:19 +0000

    Use OnAttachedToVisualTree and OnDetachedFromVisualTree

--- src/Avalonia.Xaml.Interactions/Custom/FocusOnAttachedToVisualTreeBehavior.cs
+++ src/Avalonia.Xaml.Interactions/Custom/FocusOnAttachedToVisualTreeBehavior.cs
@@ -8,32 +8,11 @@ namespace Avalonia.Xaml.Interactions.Custom;
 /// </summary>
 public class FocusOnAttachedToVisualTreeBehavior : Behavior<Control>
 {
-    /// <summary>
-    /// Called after the behavior is attached to the <see cref="Behavior.AssociatedObject"/>.
-    /// </summary>
-    protected override void OnAttached()
+    /// <inheritdoc/>
+    protected override void OnAttachedToVisualTree()
     {
-        base.OnAttached();
-        if (AssociatedObject is { })
-        {
-            AssociatedObject.AttachedToVisualTree += AssociatedObject_AttachedToVisualTree; 
-        }
-    }
+        base.OnAttachedToVisualTree();
 
-    /// <summary>
-    /// Called when the behavior is being detached from its <see cref="Behavior.AssociatedObject"/>.
-    /// </summary>
-    protected override void OnDetaching()
-    {
-        base.OnDetaching();
-        if (AssociatedObject is { })
-        {
-            AssociatedObject.AttachedToVisualTree -= AssociatedObject_AttachedToVisualTree; 
-        }
-    }
-
-    private void AssociatedObject_AttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
-    {
         AssociatedObject?.Focus();
     }
 }
