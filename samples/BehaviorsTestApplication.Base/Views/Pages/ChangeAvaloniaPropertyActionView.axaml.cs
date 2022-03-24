bbf69889c507fe615a4213a58b35d5f04ff4cacb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-03-24 13:15:55 +0100

    Add ChangeAvaloniaPropertyAction

---
+++ samples/BehaviorsTestApplication.Base/Views/Pages/ChangeAvaloniaPropertyActionView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public class ChangeAvaloniaPropertyActionView : UserControl
+{
+    public ChangeAvaloniaPropertyActionView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
