6f31608b8534c6bc5e6fdff007885cce88617c89
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-04-05 14:27:09 +0200

    Create AttachedToVisualTreeBehavior.cs

---
+++ src/Avalonia.Xaml.Interactions/Custom/AttachedToVisualTreeBehavior.cs
@@ -0,0 +1,30 @@
+using System.Reactive.Disposables;
+
+namespace Avalonia.Xaml.Interactions.Custom;
+
+/// <summary>
+/// A base class for behaviors using attached to visual tree event.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class AttachedToVisualTreeBehavior<T> : DisposingBehavior<T> where T : Visual
+{
+	private CompositeDisposable? _disposables;
+
+    /// <inheritdoc />
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		_disposables = disposables;
+	}
+
+    /// <inheritdoc />
+    protected override void OnAttachedToVisualTree()
+    {
+		OnAttachedToVisualTree(_disposables!);
+    }
+
+    /// <summary>
+    /// Called after the behavior is attached to the <see cref="DisposingBehavior{T}.AssociatedObject"/> visual tree.
+    /// </summary>
+    /// <param name="disposable">The group of disposable resources that are disposed together</param>
+	protected abstract void OnAttachedToVisualTree(CompositeDisposable disposable);
+}
