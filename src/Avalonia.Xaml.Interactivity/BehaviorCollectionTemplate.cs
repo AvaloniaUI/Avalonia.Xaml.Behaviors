3fc4ff658d69de9fc3e6d415b771ea2e0003860a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-27 18:09:27 +0100

    Update BehaviorCollectionTemplate.cs

--- src/Avalonia.Xaml.Interactivity/BehaviorCollectionTemplate.cs
+++ src/Avalonia.Xaml.Interactivity/BehaviorCollectionTemplate.cs
@@ -5,20 +5,20 @@ using Avalonia.Styling;
 namespace Avalonia.Xaml.Interactivity;
 
 /// <summary>
-/// 
+/// A template for creating a collection of <see cref="Behavior"/> objects.
 /// </summary>
 public class BehaviorCollectionTemplate : ITemplate
 {
     /// <summary>
-    /// 
+    /// Gets or sets the content of the template.
     /// </summary>
     [Content]
     [TemplateContent(TemplateResultType = typeof(BehaviorCollection))]
     public object? Content { get; set; }
 
     /// <summary>
-    /// 
+    /// Builds the collection of behaviors from the template.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>The collection of behaviors created from the template.</returns>
     object ITemplate.Build() => TemplateContent.Load<BehaviorCollection>(Content).Result;
 }
