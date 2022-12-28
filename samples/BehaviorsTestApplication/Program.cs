ec3951411a2cc5c31376cb5ff4d78125df80e144
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-28 11:33:57 +0100

    Update samples

--- samples/BehaviorsTestApplication/Program.cs
+++ samples/BehaviorsTestApplication/Program.cs
@@ -20,9 +20,6 @@ class Program
         GC.KeepAlive(typeof(ComparisonConditionType).Assembly);
         return AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .With(new Win32PlatformOptions { UseCompositor = true })
-            .With(new X11PlatformOptions { UseCompositor = true })
-            .With(new AvaloniaNativePlatformOptions { UseCompositor = true })
             .UseReactiveUI()
             .LogToTrace();
     }
