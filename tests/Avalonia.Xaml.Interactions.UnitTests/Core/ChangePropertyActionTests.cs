cf16698ba8376b00605664aaeaf1463a9a3a5dbb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-26 11:52:45 +0200

    Use xaml user control

--- tests/Avalonia.Xaml.Interactions.UnitTests/Core/ChangePropertyActionTests.cs
+++ tests/Avalonia.Xaml.Interactions.UnitTests/Core/ChangePropertyActionTests.cs
@@ -1,9 +1,6 @@
-﻿using Avalonia.Controls;
-using Avalonia.Headless;
+﻿using Avalonia.Headless;
 using Avalonia.Headless.XUnit;
 using Avalonia.Input;
-using Avalonia.Xaml.Interactions.Core;
-using Avalonia.Xaml.Interactivity;
 using Xunit;
 
 namespace Avalonia.Xaml.Interactions.UnitTests.Core;
@@ -11,51 +8,16 @@ namespace Avalonia.Xaml.Interactions.UnitTests.Core;
 public class ChangePropertyActionTests
 {
     [AvaloniaFact]
-    public void Should_Update_Text_Property_On_Click_Event()
+    public void ChangePropertyAction_001()
     {
-        var textBox = new TextBox
-        {
-            Text = "Initial Text"
-        };
-
-        var button = new Button
-        {
-            [Interaction.BehaviorsProperty] = new BehaviorCollection 
-            {
-                new EventTriggerBehavior
-                {
-                    EventName = "Click",
-                    Actions =
-                    {
-                        new ChangePropertyAction 
-                        { 
-                            TargetObject = textBox,
-                            PropertyName = "Text",
-                            Value = "Updated Text"
-                        }
-                    }
-                }
-            }
-        };
-
-        var window = new Window
-        {
-            Content = new StackPanel
-            {
-                Children =
-                {
-                    textBox,
-                    button
-                }
-            }
-        };
+        var window = new ChangePropertyAction001();
 
         window.Show();
 
         // Click
-        button.Focus();
+        window.TargetButton.Focus();
         window.KeyPress(Key.Enter, RawInputModifiers.None);
 
-        Assert.Equal("Updated Text", textBox.Text);
+        Assert.Equal("Updated Text", window.TargetTextBox.Text);
     }
 }
