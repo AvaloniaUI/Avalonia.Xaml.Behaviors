ffca2a2d660a85d3bfd2b092d3d05b9376976fc1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-28 13:41:18 +0200

    Add InvokeCommandAction unit tests

---
+++ tests/Avalonia.Xaml.Interactions.UnitTests/Core/InvokeCommandAction001.axaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Avalonia.Controls;
+
+namespace Avalonia.Xaml.Interactions.UnitTests.Core;
+
+public partial class InvokeCommandAction001 : Window
+{
+    public ICommand TestCommand { get; set; }
+
+    public InvokeCommandAction001()
+    {
+        InitializeComponent();
+
+        TestCommand = new Command(_ =>
+        {
+            TargetTextBox.Text = "Command Text";
+        });
+
+        DataContext = this;
+    }
+}
