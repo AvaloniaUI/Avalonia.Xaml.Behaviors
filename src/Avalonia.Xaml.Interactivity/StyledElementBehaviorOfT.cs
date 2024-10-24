8604ae6ff30bcaed10854de6ca0c2d2f53506bbe
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-10-24 13:33:40 +0200

    Create StyledElementBehaviorOfT.cs

---
+++ src/Avalonia.Xaml.Interactivity/StyledElementBehaviorOfT.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Avalonia.Xaml.Interactivity;
+
+/// <summary>
+/// A base class for behaviors making them code compatible with older frameworks,
+/// and allow for typed associated objects.
+/// </summary>
+/// <typeparam name="T">The object type to attach to</typeparam>
+public abstract class StyledElementBehavior<T> : StyledElementBehavior where T : AvaloniaObject
+{
+    /// <summary>
+    /// Gets the object to which this behavior is attached.
+    /// </summary>
+    public new T? AssociatedObject => base.AssociatedObject as T;
+
+    /// <summary>
+    /// Called after the behavior is attached to the <see cref="StyledElementBehavior.AssociatedObject"/>.
+    /// </summary>
+    /// <remarks>
+    /// Override this to hook up functionality to the <see cref="StyledElementBehavior.AssociatedObject"/>
+    /// </remarks>
+    [RequiresUnreferencedCode("This functionality is not compatible with trimming.")]
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        if (AssociatedObject is null && base.AssociatedObject is not null)
+        {
+            var actualType = base.AssociatedObject?.GetType().FullName;
+            var expectedType = typeof(T).FullName;
+            var message = $"AssociatedObject is of type {actualType} but should be of type {expectedType}.";
+            throw new InvalidOperationException(message);
+        }
+    }
+}
